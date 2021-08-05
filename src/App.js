4a1adcb7bbd56f0e517400bef49e06bed2d272ce
unknown <hyder1@gmail.com>
2021-08-05 09:24:59 -0400

    comments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import Details from "./Details";
 function App() {
+  //users will hold the users returned from the api call
   const [users, setUsers] = useState([]);
+
+  // appIndex will hold the index of the item being clicked on
+  // we start it at the first index 0
   const [appIndex, setAppIndex] = useState(0);
 
+  // Make api call to get data. 
   useEffect(() => {
     const url = "https://gorest.co.in/public/v1/users?page=5";
     fetch(url)
--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,13 @@
 import { useState, useEffect } from 'react'
 const Details = ({user, appIndex, setAppIndex, index}) => {
+    // hold the id of the current set of todos
+    // here I will get the todo with id 105
     const [id, setId] = useState(105)
+
+    // holds the todo data from the api call
     const [todos, setTodos] = useState([])
 
+    // when the id changes, a new api call will be made
     useEffect(() => {
         const url = `https://gorest.co.in/public/v1/users/${id}/todos`
         fetch(url)
@@ -10,6 +15,7 @@ const Details = ({user, appIndex, setAppIndex, index}) => {
         .then(({data}) => setTodos(data))
     },[id])
 
+    // updates the current id and the current index
     const handleClick = (id) => {
         setId(id)
         setAppIndex(index)
@@ -21,18 +27,20 @@ const Details = ({user, appIndex, setAppIndex, index}) => {
             <p>{user.email}</p>
             <p>{user.gender}</p>
             {  
-                appIndex === index && <section style={{marginLeft: '50px'}}>
+                // only display a todo if the indexes match
+                appIndex === index && 
+                <section style={{marginLeft: '50px'}}>
                     <h3>Todos:</h3>
-                {
-                    todos.map(todo => (
-                        <>
-                            <p>Title: {todo.title}</p>
-                            <p>Status: {todo.status}</p>
-                            <hr />
-                        </>
-                    ))
-                }
-            </section>
+                    {
+                        todos.map(todo => (
+                            <>
+                                <p>Title: {todo.title}</p>
+                                <p>Status: {todo.status}</p>
+                                <hr />
+                            </>
+                        ))
+                    }
+                </section>
             }
         </div>
     )
